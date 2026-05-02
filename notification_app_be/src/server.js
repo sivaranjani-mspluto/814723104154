4ae8abc33a3531fc68d8a07ff0ab5cbdf4326c51
Sivaranjani S <161628477+sivaranjani-mspluto@users.noreply.github.com>
2026-05-02 12:55:08 +0530

    Update server.js

--- notification_app_be/src/server.js
+++ notification_app_be/src/server.js
@@ -3,22 +3,19 @@ const cors = require("cors");
 require("dotenv").config();
 
 const notificationRoutes = require("./routes/notificationRoutes");
-
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/notifications", notificationRoutes);
 
-// Health check
 app.get("/", (req, res) => {
   res.json({ message: "Notification API is running!" });
 });
 
-// Start server
+// start server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
