6d71ad672661c574b8b906777a9796b4cb84bcf2
Sivaranjani S <161628477+sivaranjani-mspluto@users.noreply.github.com>
2026-05-02 12:54:29 +0530

    Update notificationService.js

--- notification_app_be/src/notificationService.js
+++ notification_app_be/src/notificationService.js
@@ -3,7 +3,6 @@ require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") }
 
 const NOTIFICATIONS_API = "http://20.207.122.201/evaluation-service/notifications";
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN || "";
-
 const TYPE_WEIGHT = {
   Placement: 3,
   Result: 2,
@@ -21,24 +20,19 @@ async function fetchNotifications(page = 1, limit = 20, notification_type) {
     });
 
     return response.data.notifications;
-
   } catch (err) {
     throw err;
   }
 }
-
 function calculateScore(notification) {
   const typeWeight = TYPE_WEIGHT[notification.Type] || 0;
   const recencyScore = new Date(notification.Timestamp).getTime() / 1e12;
   return typeWeight + recencyScore;
 }
-
 async function getTopNNotifications(n = 10) {
   const notifications = await fetchNotifications(1, 10);
-
-  console.log("Total fetched:", notifications.length); 
-  console.log("Sample:", notifications[0]); 
-
+  //console.log("Total fetched:", notifications.length); 
+  //console.log("Sample:", notifications[0]); 
   const scored = notifications.map((notif) => ({
     ...notif,
     score: calculateScore(notif),
@@ -47,5 +41,4 @@ async function getTopNNotifications(n = 10) {
   scored.sort((a, b) => b.score - a.score);
   return scored.slice(0, n);
 }
-
 module.exports = { fetchNotifications, getTopNNotifications };
